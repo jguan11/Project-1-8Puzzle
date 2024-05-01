371a3174cd9df130b9fab08372eea079a370f759
ccarl008 <ccarl008@gmail.com>
2024-04-30 19:25:04 -0700

    just comments

---
+++ main.cpp
@@ -0,0 +1,7 @@
+/* 
+    1) Problem Class Implementation
+    2) A* Search Implementation
+    3) Puzzle State Represenation
+    4) Testing and Debugging
+    5) Integration and Main Program (GUI)
+*/
