e7f0becbed0b325d3e8814c11079ca3699d9e20c
jguan11 <147004616+jguan11@users.noreply.github.com>
2024-05-05 20:13:57 -0700

    Add files via upload
    got rid of mahattan, added euclidean, modified astar to include euclidean instead of mahattan

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <unordered_set>
 #include <algorithm>
 #include <cstring>
+#include <cmath>
 
 #include "operators.h"
 
@@ -47,16 +48,28 @@ int misplacedHeur(int init[3][3], int goal[3][3])
     return misplaced;
 }
 
-int manhattanHeur(int init[3][3], int goal[3][3])
+double euclideanHeur(int init[3][3], int goal[3][3])
 {
-    int ret = 0;
+    double ret = 0;
     for (int i = 0; i < 3; i++)
+    {
         for (int j = 0; j < 3; j++)
+        {
             if (init[i][j] != 0)
+            {
                 for (int k = 0; k < 3; k++)
+                {
                     for (int l = 0; l < 3; l++)
+                    {
                         if (init[i][j] == goal[k][l])
-                            ret += abs(i - k) + abs(j - l);
+                        {
+                            ret += sqrt(pow(i - k, 2) + pow(j - l, 2));
+                        }
+                    }
+                }
+            }
+        }
+    }
     return ret;
 }
 
@@ -95,24 +108,14 @@ int nodesExp = 0;
 int row[] = {1, 0, -1, 0};
 int col[] = {0, -1, 0, 1};
 
-void Astar(int init[3][3], int x, int y, int goal[3][3], int heur)
+void UniformCost(int init[3][3], int x, int y, int goal[3][3])
 {
     vector<Node *> openset;
     vector<Node *> closedset;
     vector<Node *>::iterator it;
     Node *current;
 
-    int heuristic;
-    if (heur == 2)
-    {
-        heuristic = misplacedHeur(init, goal);
-    }
-    else if (heur == 3)
-    {
-        heuristic = manhattanHeur(init, goal);
-    }
-
-    Node *node = newNode(init, x, y, x, y, heuristic, 0, NULL);
+    Node *node = newNode(init, x, y, x, y, 0, 0, NULL);
     openset.push_back(node);
     qSize = openset.size();
 
@@ -140,12 +143,70 @@ void Astar(int init[3][3], int x, int y, int goal[3][3], int heur)
                 Node *child = newNode(current->matrix, current->x,
                                       current->y, current->x + row[i],
                                       current->y + col[i],
-                                      heuristic, current->g + 1, current);
+                                      0, current->g + 1, current);
 
-                if (heur == 2)
+                it = find(openset.begin(), openset.end(), child);
+                if (it == openset.end())
+                    openset.push_back(child);
+            }
+        }
+    }
+}
+
+void Astar(int init[3][3], int x, int y, int goal[3][3], int heur)
+{
+    vector<Node *> openset;
+    vector<Node *> closedset;
+    vector<Node *>::iterator it;
+    Node *current;
+
+    double heuristic = 0;
+    if (heur == 1)
+    {
+        heuristic = 0;
+    }
+    else if (heur == 2)
+    {
+        heuristic = misplacedHeur(init, goal);
+    }
+    else if (heur == 3)
+    {
+        heuristic = euclideanHeur(init, goal);
+    }
+
+    Node *node = newNode(init, x, y, x, y, heuristic, 0, NULL);
+    openset.push_back(node);
+    qSize = openset.size();
+
+    while (!openset.empty())
+    {
+        sort(openset.begin(), openset.end(), smallestF);
+        current = openset[0];
+
+        if (current->h == 0)
+        {
+            printPath(current);
+            return;
+        }
+
+        openset.erase(openset.begin());
+        closedset.push_back(current);
+        if (qSize < openset.size())
+            qSize = openset.size();
+        nodesExp++;
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (inBounds(current->x + row[i], current->y + col[i]))
+            {
+                Node *child = newNode(current->matrix, current->x, current->y, current->x + row[i], current->y + col[i], heuristic, current->g + 1, current);
+
+                if (heur == 1)
+                    child->h = 0;
+                else if (heur == 2)
                     child->h = misplacedHeur(child->matrix, goal);
                 else if (heur == 3)
-                    child->h = manhattanHeur(child->matrix, goal);
+                    child->h = euclideanHeur(child->matrix, goal);
 
                 child->f = child->h + child->g;
                 it = find(openset.begin(), openset.end(), child);
@@ -158,6 +219,12 @@ void Astar(int init[3][3], int x, int y, int goal[3][3], int heur)
 
 int main()
 {
+
+    int heur, x, y;
+    cout << "Type 1 for Uniformed Cost Search, 2 for Misplaced Tiles, and 3 for Euclidean Distance... \n";
+    cin >> heur;
+    cout << endl;
+
     int init[3][3] =
         {
             {1, 2, 3},
